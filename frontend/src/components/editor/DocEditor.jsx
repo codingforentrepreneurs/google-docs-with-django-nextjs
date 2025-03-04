e150a0eefb1c2419d114de5ceb4dba0d2a506cf3
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-03-04 15:47:48 -0700

    5 - Adding new features

--- frontend/src/components/editor/DocEditor.jsx
+++ frontend/src/components/editor/DocEditor.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef, useMemo } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import { ClassicEditor, AutoLink, Autosave, Bold, Essentials, Italic, Link, Paragraph, Underline } from 'ckeditor5';
+import { ClassicEditor, AutoLink, Autosave, Bold, Essentials, Heading, Italic, Link, Paragraph, Underline } from 'ckeditor5';
 
 import 'ckeditor5/ckeditor5.css';
 
@@ -30,10 +30,55 @@ export default function DocEditor({ref, initialData, placeholder}) {
 		return {
 			editorConfig: {
 				toolbar: {
-					items: ['bold', 'italic', 'underline', '|', 'link'],
+					items: ['heading', 'bold', 'italic', 'underline', '|', 'link'],
 					shouldNotGroupWhenFull: false
 				},
-				plugins: [AutoLink, Autosave, Bold, Essentials, Italic, Link, Paragraph, Underline],
+				plugins: [AutoLink, Autosave, Bold, Essentials, Heading, Italic, Link, Paragraph, Underline],
+				heading: {
+					options: [
+						{
+							model: 'paragraph',
+							title: 'Paragraph',
+							class: 'ck-heading_paragraph'
+						},
+						{
+							model: 'heading1',
+							view: 'h1',
+							title: 'Heading 1',
+							class: 'ck-heading_heading1'
+						},
+						{
+							model: 'heading2',
+							view: 'h2',
+							title: 'Heading 2',
+							class: 'ck-heading_heading2'
+						},
+						{
+							model: 'heading3',
+							view: 'h3',
+							title: 'Heading 3',
+							class: 'ck-heading_heading3'
+						},
+						{
+							model: 'heading4',
+							view: 'h4',
+							title: 'Heading 4',
+							class: 'ck-heading_heading4'
+						},
+						{
+							model: 'heading5',
+							view: 'h5',
+							title: 'Heading 5',
+							class: 'ck-heading_heading5'
+						},
+						{
+							model: 'heading6',
+							view: 'h6',
+							title: 'Heading 6',
+							class: 'ck-heading_heading6'
+						}
+					]
+				},
 				initialData: initialData ? initialData: '',
 				licenseKey: LICENSE_KEY,
 				link: {
